6a39d2fe2bf2a69d047bd3724f96fdabaf0ffa04
storiesbyvicky <mypostbox1896@gmail.com>
2026-06-30 21:44:24 +0530

    WWCNT updates

---
+++ assets/js/wwcnt.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("openLine");
+const universe = document.getElementById("universe");
+
+button.addEventListener("click", () => {
+
+    document.body.classList.add("crossing");
+
+    setTimeout(() => {
+
+        universe.classList.add("open");
+
+    }, 2600);
+
+});
