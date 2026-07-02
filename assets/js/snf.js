e84c0f3e644845fd1f9d0e522c376183daadd15f
storiesbyvicky <mypostbox1896@gmail.com>
2026-07-02 21:40:41 +0530

    SNF updated

---
+++ assets/js/snf.js
@@ -0,0 +1,47 @@
+// =====================================
+// OPEN THE DIARY
+// =====================================
+
+const diaryButton = document.getElementById("openDiary");
+
+if (diaryButton) {
+
+    diaryButton.addEventListener("click", () => {
+
+        document.getElementById("memory-one").scrollIntoView({
+
+            behavior: "smooth"
+
+        });
+
+    });
+
+}
+
+// =====================================
+// MEMORY FADE
+// =====================================
+
+const memories = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{threshold:.2});
+
+memories.forEach(section=>{
+
+    section.classList.add("fade-memory");
+
+    observer.observe(section);
+
+});
